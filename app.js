f71b972b9e739c689bd8283f1c9247f4475067b6
Toan Phan <Toan Phan>
2019-01-25 20:30:00 +0700

    update

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error',{
-    title: 'Errors',
+    title: 'Errors Can be see',
     path: '/error'
   });
 });
